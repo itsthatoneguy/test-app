831d2211d6e5355d186fc984a1ace79f718310f8
itsthatoneguy <itsthatoneguy@nowhere>
2012-10-03 16:12:45 -0700

    Add hostname, arch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @platform = `cat /etc/issue | head -1 | awk '{print $1}'`.chomp
+    @arch     = `arch`.chomp
+    @hostname = `hostname --short`
   end
 end
